ecca04e83f7d8032d600e18118a864abe65db20b
Igor Morzan <igormorzan@dhcp-10-250-73-45.harvard.edu>
2019-02-28 00:45:32 -0500

    filters and cleans the data

--- ps_4.R
+++ ps_4.R
@@ -1,4 +1,17 @@
+library(gt)
 library(tidyverse)
+library(janitor)
+library(lubridate)
 
-data <- read_csv("ps_4_elections-poll-nc09-3.csv") %>%
-  View()
+data <- read_csv(file = "ps_4_elections-poll-nc09-3.csv",
+  col_types = cols(
+    .default = col_character(),
+    turnout_scale = col_double(),
+    turnout_score = col_double(),
+    w_LV = col_double(),
+    w_RV = col_double(),
+    final_weight = col_double(),
+    timestamp = col_datetime(format = ""))) %>%
+    clean_names() %>%
+    filter(!is.na(response), !is.na(race_eth), !is.na(final_weight), educ != "[DO NOT READ] Refused")
+
